eefd95a7b498a3dc66aa60e11082de28b0430127
tpranjic-tgm <tpranjic@student.tgm.ac.at>
2024-03-11 20:52:06 +0100

    package.json cahnged

---
+++ dist/game.js
@@ -0,0 +1,133 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Game = void 0;
+class Game {
+    constructor(server, code) {
+        this.io = server;
+        this.code = code;
+        this.running = false;
+        this.players = new Map();
+        this.state = "init";
+        this.fPlayers = [];
+        this.cRounds = [];
+        this.cR = 0;
+        return this;
+    }
+    submitPlayer(socket, p) {
+        this.getPlayerBySocket(socket).fplayer = p;
+        let status = [];
+        this.getAllPlayers().forEach((p, name, _map) => {
+            if (p.fplayer != "") {
+                status.push(name);
+            }
+        });
+        this.fPlayers.push(p);
+        this.broadcast({
+            cmd: "picking-status",
+            data: status,
+        });
+        if (status.length == this.players.size) {
+            this.calculateRounds();
+            this.setState("disc");
+        }
+    }
+    calculateRounds() {
+        let rounds = this.players.size;
+        for (let i = 0; i < rounds; i++) {
+            let values = [...this.getAllPlayers().values()];
+            let imposter;
+            do {
+                imposter = values[~~(Math.random() * values.length)];
+            } while (imposter.fplayer == this.fPlayers[i]);
+            let round = {
+                imposter: imposter,
+                fplayer: this.fPlayers[i],
+            };
+            this.cRounds.push(round);
+        }
+    }
+    execRound() {
+        console.log(this.cRounds);
+        console.log(this.cR);
+        let imposter_name = this.cRounds[this.cR].imposter.name;
+        this.cRounds[this.cR].imposter.socket.emit("packet", {
+            cmd: "round",
+            data: "imposter",
+        });
+        this.players.forEach((p, name, _map) => {
+            if (name == imposter_name)
+                return;
+            p.socket.emit("packet", {
+                cmd: "round",
+                data: this.fPlayers[this.cR],
+            });
+        });
+    }
+    nextRound() {
+        if (this.cR == this.players.size - 1) {
+            this.fPlayers = [];
+            this.cR = 0;
+            this.cRounds = [];
+            this.getAllPlayers().forEach((p, name, _map) => {
+                p.fplayer = "";
+            });
+            this.setState("picking");
+        }
+        else {
+            this.cR++;
+            this.execRound();
+        }
+    }
+    addPlayer(name, socket, owner) {
+        let player = {
+            name: name,
+            socket: socket,
+            owner: owner,
+            fplayer: "",
+        };
+        this.players.set(name, player);
+    }
+    getPlayerByName(name) {
+        return this.players.get(name);
+    }
+    getPlayerBySocket(socket) {
+        let p = undefined;
+        this.players.forEach((value, key, _map) => {
+            console.log(key);
+            console.log(socket === value.socket);
+            if (socket === value.socket) {
+                p = value;
+            }
+        });
+        return p;
+    }
+    removePlayerByName(name) {
+        this.players.delete(name);
+    }
+    getAllPlayers() {
+        return this.players;
+    }
+    getRunning() {
+        return this.running;
+    }
+    setRunning(b) {
+        this.running = b;
+        this.broadcast({ cmd: "running", data: b });
+        this.setState("picking");
+    }
+    getState() {
+        return this.state;
+    }
+    setState(s) {
+        this.state = s;
+        this.broadcast({ cmd: "state", data: s });
+        if (s == "disc") {
+            this.execRound();
+        }
+    }
+    broadcast(msg) {
+        console.log(msg);
+        this.io.to(this.code).emit("packet", msg);
+    }
+}
+exports.Game = Game;
---
+++ dist/gameserver.js
@@ -0,0 +1,138 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.GameServer = void 0;
+const socketio = __importStar(require("socket.io"));
+const events = __importStar(require("events"));
+const game_1 = require("./game");
+class GameServer {
+    constructor(server) {
+        this.io = new socketio.Server(server, {
+            cors: {
+                origin: "*",
+                methods: ["GET", "POST"],
+            },
+        });
+        this.games = new Map();
+        this.listen();
+        this.ev = new events.EventEmitter();
+        return this;
+    }
+    listen() {
+        this.io.on("connect", (socket) => {
+            let _this = this;
+            let gameCode = "";
+            socket.on("join", (data) => {
+                var _a, _b;
+                if (this.io.sockets.adapter.rooms.has(data.code)) {
+                    socket.join(data.code);
+                    (_a = this.games.get(data.code)) === null || _a === void 0 ? void 0 : _a.addPlayer(data.name, socket, false);
+                    if (((_b = this.games.get(data.code)) === null || _b === void 0 ? void 0 : _b.getRunning()) == true) {
+                        socket.emit("err", "game-already-started");
+                    }
+                    else {
+                        gameCode = data.code;
+                        socket.emit("join-success");
+                    }
+                }
+                else {
+                    socket.emit("err", "game-not-found");
+                }
+            });
+            socket.on("create", (name) => {
+                var _a;
+                let code;
+                do {
+                    code = Math.floor(1000 + Math.random() * 9000).toString();
+                } while ((_a = this.io.sockets.adapter.rooms.get(code)) === null || _a === void 0 ? void 0 : _a.size);
+                socket.join(code);
+                let game = new game_1.Game(this.io, code);
+                game.addPlayer(name, socket, true);
+                this.games.set(code, game);
+                gameCode = code;
+                socket.emit("create-success", code);
+            });
+            socket.on("check", (code) => {
+                var _a, _b;
+                if (!socket.rooms.has(code)) {
+                    socket.emit("err", "not-part-of-game");
+                }
+                else {
+                    let msg = ((_a = this.io.sockets.adapter.rooms.get(code)) === null || _a === void 0 ? void 0 : _a.size.toString()) + " | ";
+                    (_b = this.games
+                        .get(code)) === null || _b === void 0 ? void 0 : _b.getAllPlayers().forEach((_value, key, _map) => {
+                        msg += key + " ";
+                    });
+                    this.io.to(code).emit("size", msg);
+                }
+            });
+            socket.on("packet", (data) => {
+                _this.onPacket(socket, gameCode, data);
+            });
+            socket.on("disconnecting", () => {
+                socket.rooms.forEach((__, key, _) => {
+                    var _a, _b, _c, _d;
+                    if (key.length == 4) {
+                        let p = (_a = this.games.get(key)) === null || _a === void 0 ? void 0 : _a.getPlayerBySocket(socket);
+                        (_b = this.games.get(key)) === null || _b === void 0 ? void 0 : _b.removePlayerByName(p.name);
+                        let size = (_c = this.io.sockets.adapter.rooms.get(key)) === null || _c === void 0 ? void 0 : _c.size;
+                        size = size - 1;
+                        let msg = size + " | ";
+                        (_d = this.games
+                            .get(key)) === null || _d === void 0 ? void 0 : _d.getAllPlayers().forEach((_value, key, _map) => {
+                            msg += key + " ";
+                        });
+                        this.io.to(key).emit("size", msg);
+                        if (size == 0) {
+                            this.games.delete(key);
+                        }
+                    }
+                });
+            });
+            socket.on("disconnect", this.onDisconnect);
+        });
+    }
+    onPacket(socket, code, data) {
+        console.log(data.cmd);
+        switch (data.cmd) {
+            case "changeGameRunning":
+                this.games.get(code).setRunning(data.data);
+                break;
+            case "submit-player":
+                if (this.games.get(code).getState() != "picking")
+                    break;
+                this.games.get(code).submitPlayer(socket, data.data);
+                break;
+            case "disc-stop":
+                this.games.get(code).nextRound();
+                break;
+        }
+        //socket.broadcast.to(code).emit("message", data);
+    }
+    onDisconnect(m) {
+        console.log("TCL: SocketIO -> privateonDisconnect -> m", m);
+    }
+}
+exports.GameServer = GameServer;
